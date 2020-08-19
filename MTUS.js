d2c467bf11dac26f46b428668a83cfcd0414cfd6
lovelyCARDINAL <69918945+lovelyCARDINAL@users.noreply.github.com>
2020-08-20 03:29:57 +0800

    Create MTUS.js

---
+++ MTUS.js
@@ -0,0 +1,92 @@
+// 巡查用小工具，对于主空间条目，在右上角【更多】中添加【打回用户页】，当条目贡献者不只一人时有额外提示。
+
+$(function() {
+    var self = $('#p-cactions .menu ul');
+    if (!self.find('li')[0] || $('.willBeDeleted')[0] || mw.config.get('wgUserGroups').indexOf('patroller') === -1 || mw.config.get('wgNamespaceNumber') != 0) return;
+    $('<a/>', {
+        attr: {
+            href: "#",
+            title: "移动到创建者的用户子页并挂删[alt-shift-m]",
+            accesskey: 'm'
+        },
+        text: '打回家'
+    }).on('click', function() {
+        var api = new mw.Api();
+        api.get({
+            action: 'query',
+            format: 'json',
+            prop: 'contributors',
+            titles: mw.config.get('wgPageName'),
+        }).then(function(d) {
+            if (d.error) {
+                alert('查询贡献信息失败！');
+                return;
+            }
+            var contributors = d.query.pages[mw.config.get('wgArticleId')].contributors;
+            if (contributors.length != 1 && confirm('贡献者并非只有创建者一人，请检查页面历史。确定移动回创建者用户页？') == false) return;
+
+            var default_reason = '移动回用户页残留重定向'
+            var reason = prompt('打回用户页的理由将会作为移动原因和挂删理由\n空白则使用默认（' + default_reason + '）\n取消则不打回：'),
+                self = $(this);
+            if (reason === null) return;
+            if (reason === '') reason = default_reason;
+            var loadingBox = $('<div/>', {
+                css: {
+                    position: 'fixed',
+                    top: '0',
+                    left: '0',
+                    height: '100vh',
+                    width: '100vw',
+                    transition: 'opacity .73s linear',
+                    color: 'black',
+                    'padding-top': '49vh',
+                    'background-color': 'rgba(255,255,255,0.73)',
+                    'text-align': 'center'
+                }
+            }).append('<img src="https://static.mengniang.org/common/d/d1/Windows_10_loading.gif" style="height: 1em; margin-top: -.25em;">正在打回中……');
+            var reasonText = reason ? '|' + reason : '';
+            loadingBox.endOut = function endOut() {
+                this.css('color', 'red').text('打回失败……').delay(1000).queue(function() {
+                    $(this).css('opacity', '0').delay(730).queue(function() {
+                        $(this).remove();
+                        $(document.body).css('overflow', 'auto');
+                        $(this).dequeue();
+                    });
+                    $(this).dequeue();
+                });
+            };
+            $(document.body).append(loadingBox).css('overflow', 'hidden');       
+            api.postWithToken('csrf', {
+                action: 'move',
+                format: 'json',
+                from: mw.config.get('wgPageName'),
+                to: 'User:' + contributors[contributors.length - 1].name + '/' + mw.config.get('wgPageName'),
+                reason: reason,
+                watchlist: 'preferences'
+            }).then(function(d) {
+                if (d.error) return loadingBox.endOut();
+                api.postWithToken('csrf', {
+                    action: 'edit',
+                    format: 'json',
+                    title: mw.config.get('wgPageName'),
+                    text: '<noinclude>{{即将删除' + reasonText + '|user=' + mw.config.get("wgUserName") + '}}</noinclude>',
+                    summary: '移除重定向：' + reason,
+                    nocreate: true,
+                    watchlist: 'preferences'
+                }).then(function(d) {
+                    if (d.error) return loadingBox.endOut();
+                    loadingBox.css('color', 'green').text('打回成功！即将刷新……');
+                    window.setTimeout(function() {
+                        window.location.reload();
+                    }, 730)
+                }, loadingBox.endOut.bind(loadingBox));
+            }, loadingBox.endOut.bind(loadingBox));
+        }, function() {
+            alert('打回失败！');
+        });
+    }).appendTo($('<li/>', {
+        attr: {
+            id: 'ca-moveToUserSubpage'
+        }
+    }).prependTo(self));
+});
