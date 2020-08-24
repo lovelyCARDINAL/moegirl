25b4918b42a1471ead60f0e8ba19c1fe25f0f84f
lovelyCARDINAL <69918945+lovelyCARDINAL@users.noreply.github.com>
2020-08-25 02:20:27 +0800

    Update XZ.js

--- XZ.js
+++ XZ.js
@@ -3,7 +3,7 @@ document.body.onload = function() {
     if (window.mw && !window.xzMwLoaded) {
         mw.loader.load('https://zh.moegirl.org.cn/MediaWiki:Group-sysop.js?action=raw&ctype=text/javascript');
         $("<style/>").html([
-            "https://zh.moegirl.org.cn/MediaWiki:Group-sysop.css?action=raw&ctype=text/css"
+            "https://cdn.jsdelivr.net/gh/lovelyCARDINAL/moegirl@master/SS.css"
         ].map(x => "@import url(\"" + x + "\");").join("\n")).appendTo($("head"));
         window.xzMwLoaded = true;
     }
