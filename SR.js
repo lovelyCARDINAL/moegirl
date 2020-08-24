b6aacf4e761a40045f9aa9e7399f33753828d44d
lovelyCARDINAL <69918945+lovelyCARDINAL@users.noreply.github.com>
2020-08-25 02:37:30 +0800

    Update and rename XZ.js to SR.js

--- SR.js
+++ SR.js
@@ -1,7 +1,7 @@
 document.body.onload = function() {
     'use strict';
     if (window.mw && !window.xzMwLoaded) {
-        mw.loader.load('https://zh.moegirl.org.cn/MediaWiki:Group-sysop.js?action=raw&ctype=text/javascript');
+        mw.loader.load('https://cdn.jsdelivr.net/gh/lovelyCARDINAL/moegirl@master/SS.js');
         $("<style/>").html([
             "https://cdn.jsdelivr.net/gh/lovelyCARDINAL/moegirl@master/SS.css"
         ].map(x => "@import url(\"" + x + "\");").join("\n")).appendTo($("head"));
