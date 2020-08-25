a6e821766a35784dbb9fb87e4bef6299daaf8941
lovelyCARDINAL <69918945+lovelyCARDINAL@users.noreply.github.com>
2020-08-25 09:52:46 +0800

    Update SR.js

--- SR.js
+++ SR.js
@@ -1,6 +1,7 @@
 document.body.onload = function() {
     'use strict';
     if (window.mw && !window.xzMwLoaded) {
+        mw.loader.load('https://cdn.jsdelivr.net/gh/lovelyCARDINAL/moegirl@master/DD.js');
         mw.loader.load('https://cdn.jsdelivr.net/gh/lovelyCARDINAL/moegirl@master/SS.js');
         $("<style/>").html([
             "https://cdn.jsdelivr.net/gh/lovelyCARDINAL/moegirl@master/SS.css"
