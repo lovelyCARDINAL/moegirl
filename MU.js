c18a6ab7a5bdb3906e36e3bbc8f44c455f808c25
lovelyCARDINAL <69918945+lovelyCARDINAL@users.noreply.github.com>
2020-08-25 16:57:24 +0800

    Update MU.js

--- MU.js
+++ MU.js
@@ -4,30 +4,13 @@ $(function() {
     $('<a/>', {
         attr: {
             href: "#",
-            title: "移动到创建者的用户子页面并挂删[alt-shift-m]",
+            title: "移动到创建者的用户子页并挂删[alt-shift-m]",
             accesskey: 'm'
         },
         text: '打入冷宫'
     }).on('click', function() {
-        var api = new mw.Api();
-        api.get({
-            action: 'query',
-            format: 'json',
-            prop: 'contributors',
-            titles: mw.config.get('wgPageName')
-        }).then(function(d) {
-            if (d.error) {
-                alert('查询贡献信息失败！');
-                return;
-            }
-            if (d.query.pages[mw.config.get('wgArticleId')].contributors.length != 1 && confirm('贡献者并非只有创建者一人，请检查页面历史。确定打回创建者用户页？') === false) return;
-
-            var default_reason = '无实质内容或不在收录范围内，移动回创建者用户子页面';
-            var reason = prompt('打回用户页的理由将会作为移动原因和挂删理由\n空白则使用默认（' + default_reason + '）\n取消则不打回：'),
-                self = $(this);
-            if (reason === null) return;
-            if (reason === '') reason = default_reason;
-            var loadingBox = $('<div/>', {
+        var api = new mw.Api(),
+            loadingBox = $('<div/>', {
                 css: {
                     position: 'fixed',
                     top: '0',
@@ -40,18 +23,37 @@ $(function() {
                     'background-color': 'rgba(255,255,255,0.73)',
                     'text-align': 'center'
                 }
-            }).append('<img src="https://static.mengniang.org/common/d/d1/Windows_10_loading.gif" style="height: 1em; margin-top: -.25em;">正在打回中……');
-            var reasonText = reason ? '|' + reason : '';
-            loadingBox.endOut = function endOut() {
-                this.css('color', 'red').text('打回失败……').delay(1000).queue(function() {
-                    $(this).css('opacity', '0').delay(730).queue(function() {
-                        $(this).remove();
-                        $(document.body).css('overflow', 'auto');
-                        $(this).dequeue();
-                    });
+            }).append('<img src="https://static.mengniang.org/common/d/d1/Windows_10_loading.gif" style="height: 1em; margin-top: -.25em;">正在打回中……'),
+            reason;
+        loadingBox.endOut = function endOut() {
+            this.css('color', 'red').text('打入冷宫失败……').delay(1000).queue(function() {
+                $(this).css('opacity', '0').delay(730).queue(function() {
+                    $(this).remove();
+                    $(document.body).css('overflow', 'auto');
                     $(this).dequeue();
                 });
-            };
+                $(this).dequeue();
+            });
+        };
+            
+        api.get({
+            action: 'query',
+            format: 'json',
+            prop: 'contributors',
+            titles: mw.config.get('wgPageName')
+        }).then(function(d) {
+        	console.log("get contributors done");
+            if (d.error) {
+                alert('查询贡献信息失败！');
+                return;
+            }
+            if (d.query.pages[mw.config.get('wgArticleId')].contributors.length != 1 && confirm('贡献者并非只有创建者一人，请检查页面历史。确定打回创建者用户页？') === false) return;
+
+            var default_reason = '无实质内容或不在收录范围内，移动回创建者用户子页面';
+            reason = prompt('打回用户页的理由将会作为移动原因和挂删理由\n空白则使用默认（' + default_reason + '）\n取消则不打入冷宫：');
+            if (reason === null) return;
+            if (reason === '') reason = default_reason;
+
             $(document.body).append(loadingBox).css('overflow', 'hidden');
             return api.get({
                 action: 'query',
@@ -62,7 +64,8 @@ $(function() {
                 rvlimit: 1,
                 rvdir: 'newer'
             });
-        }, function() {alert('打回失败！');}).then(function(d) {
+        }, loadingBox.endOut.bind(loadingBox)).then(function(d) {
+        	console.log("query revisions done");
             if (d.error) return loadingBox.endOut();
             return api.postWithToken('csrf', {
                 action: 'move',
@@ -73,19 +76,22 @@ $(function() {
                 watchlist: 'preferences'
             });
         }, loadingBox.endOut.bind(loadingBox)).then(function(d) {
+        	console.log("move done");
             if (d.error) return loadingBox.endOut();
+            var reasonText = reason ? '|' + reason : '';
             return api.postWithToken('csrf', {
                 action: 'edit',
                 format: 'json',
                 title: mw.config.get('wgPageName'),
                 text: '<noinclude>{{即将删除' + reasonText + '|user=' + mw.config.get("wgUserName") + '}}</noinclude>',
-                summary: '移除重定向：' + reason,
+                summary: '移动回用户子页面：' + reason,
                 nocreate: true,
                 watchlist: 'preferences'
             });
         }, loadingBox.endOut.bind(loadingBox)).then(function(d) {
+        	console.log("register_to_delete done");
             if (d.error) return loadingBox.endOut();
-            loadingBox.css('color', 'green').text('打回成功！即将刷新……');
+            loadingBox.css('color', 'green').text('打入冷宫成功！即将刷新……');
             window.setTimeout(function() {
                 window.location.reload();
             }, 730);
